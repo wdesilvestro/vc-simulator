3d1df3b5e1b221645cfa68f216b9ae357bde8b75
Wes De Silvestro <wesdesilvestro@Wes-MacBook-Pro.local>
2022-05-10 13:47:34 -0400

    Added additional graphs and params.

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ with st.sidebar:
     st.title("Configure simulation parameters")
 
     st.subheader("Probability of return multiples")
-    st.markdown("*All sliders must sum to 100% in total.*")
+    st.caption("All sliders must sum to 100% in total.")
     # TODO: Throw an error if it doesn't sum to less than 100.
     input_prob_dist_zero = st.slider(label="% of companies that return 0x", min_value=1, max_value=100, step=1, value=33,
     help="The percentage of companies that are expected to return nothing on the \
@@ -35,19 +35,38 @@ with st.sidebar:
     # TODO: Reset button
     st.markdown("##")
 
-    st.subheader("Fund parameters")
+    st.subheader("Investment parameters")
     input_liquidation_pct = st.number_input(label="% of original investment \
     recovered in event of liquidation", min_value=1.0, max_value=100.0, step=5.0, value=80.0,
     help="In the event a portfolio company must be liquidated, this is the expected \
     percentage of the original investment that will be recovered.")
-    input_target_yoy_growth = st.number_input(label="Target % for YoY growth", min_value=0.1, max_value=300.0, step=1.0, value=30.0,
+    input_target_yoy_growth = st.number_input(label="Target % for YoY growth", min_value=0.1, max_value=300.0, step=1.0, value=25.0,
     help="The targeted percentage of year-over-year growth that the venture funds \
     are seeking with their investments.")
-    input_target_exit_time = st.number_input(label="Target # of years until an investment exits", min_value=1, max_value=100, step=1, value=4,
+    input_target_exit_time = st.number_input(label="Target # of years until an investment exits", min_value=1, max_value=100, step=1, value=5,
     help="The targeted number of years by which the venture fund would like to exit \
     an investment.")
+    st.markdown("##")
+
+    st.subheader("Fund parameters")
+    st.caption("Note that the Simulator assumes an equal investment into each \
+    company in the portfolio, meaning there is no need to ask about check size \
+    or total assets under management in order to calculate overall returns.")
+    st.caption("Note that the Simulator also assumes a clawback provision is \
+    present. Meaning that the VC will take no more of the profits than their \
+    performance fee indicates they are owed.")
     input_portfolio_size= st.number_input(label="Portfolio size", min_value=1, max_value=1000, step=1, value=50,
     help="The number of companies that each venture fund will invest in.")
+    input_management_fee_percent = st.number_input(label="% management fee", min_value=0.0, max_value=100.0, step=0.5, value=2.0,
+    help="The percentage of committed capital charged annually as a \
+    management fee.")
+    input_carry_percent = st.number_input(label="% performance fee (carried interest)", min_value=0.0, max_value=100.0, step=1.0, value=20.0,
+    help='The percentage of profits made on the principal investment that goes \
+    to the venture capitalist for generating a positive return. This is commonly \
+    called "carry" in the industry.')
+    input_fund_lifespan = st.number_input(label="Fund lifespan in years", min_value=1, max_value=100, step=1, value=10,
+    help="The amount of years the venture fund is expected to exist before all \
+    capital must be returned to its limited partners.")
     st.markdown("##")
 
     st.subheader("Simulation parameters")
@@ -70,17 +89,46 @@ sim_data = simulate_multiple_funds([input_prob_dist_zero / 100.0,
                                    input_portfolio_size,
                                    input_simulation_runs)
 
-sim_returns_list = calculate_overall_fund_returns(sim_data)
+raw_returns_list = calculate_raw_fund_returns(sim_data)
+actual_returns_list = calculate_actual_fund_returns(raw_returns_list,
+                                                    input_management_fee_percent
+                                                    / 100.0,
+                                                    input_fund_lifespan)
 
 st.subheader("Overview of simulated fund returns")
 st.markdown("Given `{}` venture funds with a portfolio size of `{}` companies \
-each, below is a summary of overall returns for each fund.".format(input_simulation_runs, input_portfolio_size))
-stat_col1, stat_col2, stat_col3, stat_col4, stat_col5 = st.columns(5)
-stat_col1.metric("25th Percentile Fund", "{0:.2f}x".format(np.quantile(sim_returns_list, q=0.25)))
-stat_col2.metric("50th Percentile Fund", "{0:.2f}x".format(np.quantile(sim_returns_list, q=0.50)))
-stat_col3.metric("75th Percentile Fund", "{0:.2f}x".format(np.quantile(sim_returns_list, q=0.75)))
-stat_col4.metric("90th Percentile Fund", "{0:.2f}x".format(np.quantile(sim_returns_list, q=0.90)))
-stat_col5.metric("99th Percentile Fund", "{0:.2f}x".format(np.quantile(sim_returns_list, q=0.99)))
+each, below is a summary of overall returns for each fund after management fees \
+have been deducted.".format(input_simulation_runs, input_portfolio_size))
+
+# text_col1, text_col2, text_col3, text_col4, text_col5 = st.columns(5)
+# text_col1.markdown("##### 25th Percentile")
+# text_col2.markdown("##### 50th Percentile")
+# text_col3.markdown("##### 75th Percentile")
+# text_col4.markdown("##### 90th Percentile")
+# text_col5.markdown("##### 99th Percentile")
+
+# raw_stat_col1, raw_stat_col2, raw_stat_col3, raw_stat_col4, raw_stat_col5 = st.columns(5)
+# raw_stat_col1.metric("Raw return", "{0:.1f}x".format(np.quantile(raw_returns_list, q=0.25)))
+# raw_stat_col2.metric("Raw return", "{0:.1f}x".format(np.quantile(raw_returns_list, q=0.50)))
+# raw_stat_col3.metric("Raw return", "{0:.1f}x".format(np.quantile(raw_returns_list, q=0.75)))
+# raw_stat_col4.metric("Raw return", "{0:.1f}x".format(np.quantile(raw_returns_list, q=0.90)))
+# raw_stat_col5.metric("Raw return", "{0:.1f}x".format(np.quantile(raw_returns_list, q=0.99)))
+
+
+# actual_stat_col1, actual_stat_col2, actual_stat_col3, actual_stat_col4, actual_stat_col5 = st.columns(5)
+# actual_stat_col1.metric("Post-mgmt fee", "{0:.1f}x".format(np.quantile(actual_returns_list, q=0.25)))
+# actual_stat_col2.metric("Post-mgmt fee", "{0:.1f}x".format(np.quantile(actual_returns_list, q=0.50)))
+# actual_stat_col3.metric("Post-mgmt fee", "{0:.1f}x".format(np.quantile(actual_returns_list, q=0.75)))
+# actual_stat_col4.metric("Post-mgmt fee", "{0:.1f}x".format(np.quantile(actual_returns_list, q=0.90)))
+# actual_stat_col5.metric("Post-mgmt fee", "{0:.1f}x".format(np.quantile(actual_returns_list, q=0.99)))
+
+
+actual_stat_col1, actual_stat_col2, actual_stat_col3, actual_stat_col4, actual_stat_col5 = st.columns(5)
+actual_stat_col1.metric("25th Percentile", "{0:.1f}x".format(np.quantile(actual_returns_list, q=0.25)))
+actual_stat_col2.metric("50th Percentile", "{0:.1f}x".format(np.quantile(actual_returns_list, q=0.50)))
+actual_stat_col3.metric("75th Percentile", "{0:.1f}x".format(np.quantile(actual_returns_list, q=0.75)))
+actual_stat_col4.metric("90th Percentile", "{0:.1f}x".format(np.quantile(actual_returns_list, q=0.90)))
+actual_stat_col5.metric("99th Percentile", "{0:.1f}x".format(np.quantile(actual_returns_list, q=0.99)))
 
 # Configure necessary fonts
 ssp_font_regular = font_manager.FontProperties(fname='./Source_Sans_Pro/SourceSansPro-Regular.ttf')
@@ -92,7 +140,7 @@ fig.patch.set_facecolor("#000000")
 fig.patch.set_alpha(0)
 ax.patch.set_facecolor("#000000")
 ax.patch.set_alpha(0)
-ax.hist(x=sim_returns_list, range=(1,100), rwidth=1, color="#14BAA6")
+ax.hist(x=actual_returns_list, range=(1,100), rwidth=1, color="#14BAA6")
 ax.tick_params(color='white', labelcolor="white")
 for spine in ax.spines.values():
         spine.set_edgecolor('white')
@@ -115,7 +163,147 @@ Most limited partners (LPs) that provide capital to VC funds are looking for a \
 justify investing in venture capital funds.")
 
 bm_col1, bm_col2, bm_col3, bm_col4 = st.columns(4)
-bm_col1.metric("% funds with ≥ 3x return", "{0:.1f}%".format((len([x for x in sim_returns_list if x >= 3])/len(sim_returns_list))*100))
-bm_col2.metric("% funds with 2-3x return", "{0:.1f}%".format((len([x for x in sim_returns_list if (x < 3) and (x >= 2)])/len(sim_returns_list))*100))
-bm_col3.metric("% funds with 1-2x return", "{0:.1f}%".format((len([x for x in sim_returns_list if (x < 2) and (x >= 1)])/len(sim_returns_list))*100))
-bm_col4.metric("% funds with < 1x return", "{0:.1f}%".format((len([x for x in sim_returns_list if x < 1])/len(sim_returns_list))*100))
+bm_col1.metric("% funds with ≥ 3x return", "{0:.1f}%".format((len([x for x in actual_returns_list if x >= 3])/len(actual_returns_list))*100))
+bm_col2.metric("% funds with 2-3x return", "{0:.1f}%".format((len([x for x in actual_returns_list if (x < 3) and (x >= 2)])/len(actual_returns_list))*100))
+bm_col3.metric("% funds with 1-2x return", "{0:.1f}%".format((len([x for x in actual_returns_list if (x < 2) and (x >= 1)])/len(actual_returns_list))*100))
+bm_col4.metric("% funds with < 1x return", "{0:.1f}%".format((len([x for x in actual_returns_list if x < 1])/len(actual_returns_list))*100))
+
+
+st.markdown("Below are two scatterplots, one showing funding returns with \
+multiples from 0x to 50x and the second showing fund returns from 51x and \
+greater. In the first plot, anything above the red line passes the industry \
+benchmark of achieving at least a 3x return.")
+fig2, ax2 = plt.subplots(figsize=(12, 6))
+fig2.patch.set_facecolor("#000000")
+fig2.patch.set_alpha(0)
+ax2.patch.set_facecolor("#000000")
+ax2.patch.set_alpha(0)
+ax2.scatter(x=np.arange(0,len(actual_returns_list)), y=actual_returns_list,
+            color="#14BAA6", s=3)
+plt.ylim(0,50)
+plt.axhline(y=3, color='#ef4444', linestyle='dashed', linewidth=3)
+ax2.tick_params(color='white', labelcolor="white")
+for spine in ax2.spines.values():
+        spine.set_edgecolor('white')
+plt.title("Scatterplot of Fund Return Multiples (0-50x)", color="white",
+          fontproperties=ssp_font_bold, fontsize=18)
+plt.xlabel("Fund #", color="white", fontproperties=ssp_font_semibold, fontsize=14)
+plt.ylabel("Multiple", color="white", fontproperties=ssp_font_semibold, fontsize=14)
+plt.setp(ax2.get_xticklabels(), fontproperties=ssp_font_regular, fontsize=13)
+plt.setp(ax2.get_yticklabels(), fontproperties=ssp_font_regular, fontsize=13)
+st.pyplot(fig2)
+st.write()
+
+fig3, ax3 = plt.subplots(figsize=(12, 6))
+fig3.patch.set_facecolor("#000000")
+fig3.patch.set_alpha(0)
+ax3.patch.set_facecolor("#000000")
+ax3.patch.set_alpha(0)
+ax3.scatter(x=np.arange(0,len(actual_returns_list)), y=actual_returns_list,
+            color="#14BAA6", s=50)
+plt.ylim(51,np.max(actual_returns_list) + 100)
+ax3.tick_params(color='white', labelcolor="white")
+for spine in ax3.spines.values():
+        spine.set_edgecolor('white')
+plt.title("Scatterplot of Fund Return Multiples (>50x)", color="white",
+          fontproperties=ssp_font_bold, fontsize=18)
+plt.xlabel("Fund #", color="white", fontproperties=ssp_font_semibold, fontsize=14)
+plt.ylabel("Multiple", color="white", fontproperties=ssp_font_semibold, fontsize=14)
+plt.setp(ax3.get_xticklabels(), fontproperties=ssp_font_regular, fontsize=13)
+plt.setp(ax3.get_yticklabels(), fontproperties=ssp_font_regular, fontsize=13)
+st.pyplot(fig3)
+st.markdown("###")
+
+
+fund_analysis_list = analyze_fund_returns(sim_data, raw_returns_list, input_portfolio_size)
+
+st.subheader("Where the returns are coming from")
+st.markdown("We can also investigate where the returns are coming from *within* \
+each venture fund (i.e., what investments are actually driving overall returns). \
+We'll split our simulated funds into four buckets: <1x return, 1-2x return, 2-3x \
+return, and ≥ 3x return.")
+st.markdown("First, we'll look at the *composition* of each each bucket in terms \
+of the types of companies within each fund's portfolio. Below is a chart showing \
+how many failed (<1x), breakeven (1-2x), moderately sucessful (2-3x), and winner \
+(≥3x) companies make up each bucket.")
+
+fig4, ax4 = plt.subplots(figsize=(12, 6))
+fig4.patch.set_facecolor("#000000")
+fig4.patch.set_alpha(0)
+ax4.patch.set_facecolor("#000000")
+ax4.patch.set_alpha(0)
+labels = ['Failed Fund', 'Breakeven Fund', 'Moderately Successful Fund', 'Winner Fund']
+
+pct_comp_less_1x = 100 * get_averages_for_variable_across_buckets(fund_analysis_list, 'pct_comp_less_1x')
+pct_comp_1x_2x = 100 * get_averages_for_variable_across_buckets(fund_analysis_list, 'pct_comp_1x_2x')
+pct_comp_2x_3x = 100 * get_averages_for_variable_across_buckets(fund_analysis_list, 'pct_comp_2x_3x')
+pct_comp_greateq_3x = 100 * get_averages_for_variable_across_buckets(fund_analysis_list, 'pct_comp_greateq_3x')
+
+ax4.bar(labels, pct_comp_less_1x, label='Companies returning < 1x', color="#ef4444")
+ax4.bar(labels, pct_comp_1x_2x, label='Companies returning 1-2x',
+        bottom=pct_comp_less_1x, color="#eab308")
+ax4.bar(labels, pct_comp_2x_3x, label='Companies returning 2-3x',
+        bottom=pct_comp_less_1x+pct_comp_1x_2x, color="#3b82f6")
+ax4.bar(labels, pct_comp_greateq_3x, label='Companies returning ≥3x',
+        bottom=pct_comp_less_1x+pct_comp_1x_2x+pct_comp_2x_3x, color="#22c55e")
+ax4.tick_params(color='white', labelcolor="white")
+plt.ylim(0,105,1)
+for spine in ax4.spines.values():
+        spine.set_edgecolor('white')
+plt.title("Breakdown of Fund Composition", color="white",
+          fontproperties=ssp_font_bold, fontsize=18)
+plt.xlabel("Bucket of Fund", color="white", fontproperties=ssp_font_semibold, fontsize=14)
+plt.ylabel("% of Fund Composition", color="white", fontproperties=ssp_font_semibold, fontsize=14)
+ax4.legend(loc='upper center', bbox_to_anchor=(0.5, -0.12), ncol=5,
+           frameon=False)
+plt.setp(ax4.get_legend().get_texts(), color='white', fontproperties=ssp_font_regular,
+         fontsize=12)
+plt.setp(ax4.get_xticklabels(), fontproperties=ssp_font_regular, fontsize=13)
+plt.setp(ax4.get_yticklabels(), fontproperties=ssp_font_regular, fontsize=13)
+st.pyplot(fig4)
+
+st.markdown("Next, we'll compare the *source of returns* for each bucket in \
+terms of what companies are driving overall fund performance. Below is a chart \
+showing the percentage of overall fund returns that are attributable to each \
+type of company within a particular bucket of funds.")
+
+
+fig5, ax5 = plt.subplots(figsize=(12, 6))
+fig5.patch.set_facecolor("#000000")
+fig5.patch.set_alpha(0)
+ax5.patch.set_facecolor("#000000")
+ax5.patch.set_alpha(0)
+
+pct_return_less_1x = 100 * get_averages_for_variable_across_buckets(fund_analysis_list, 'pct_return_less_1x')
+pct_return_1x_2x = 100 * get_averages_for_variable_across_buckets(fund_analysis_list, 'pct_return_1x_2x')
+pct_return_2x_3x = 100 * get_averages_for_variable_across_buckets(fund_analysis_list, 'pct_return_2x_3x')
+pct_return_greateq_3x = 100 * get_averages_for_variable_across_buckets(fund_analysis_list, 'pct_return_greateq_3x')
+
+ax5.bar(labels, pct_return_less_1x, label='Companies returning < 1x', color="#ef4444")
+ax5.bar(labels, pct_return_1x_2x, label='Companies returning 1-2x',
+        bottom=pct_return_less_1x, color="#eab308")
+ax5.bar(labels, pct_return_2x_3x, label='Companies returning 2-3x',
+        bottom=pct_return_less_1x+pct_return_1x_2x, color="#3b82f6")
+ax5.bar(labels, pct_return_greateq_3x, label='Companies returning ≥3x',
+        bottom=pct_return_less_1x+pct_return_1x_2x+pct_return_2x_3x, color="#22c55e")
+ax5.tick_params(color='white', labelcolor="white")
+plt.ylim(0,105,1)
+for spine in ax5.spines.values():
+        spine.set_edgecolor('white')
+plt.title("Breakdown of Fund Returns", color="white",
+          fontproperties=ssp_font_bold, fontsize=18)
+plt.xlabel("Bucket of Fund", color="white", fontproperties=ssp_font_semibold, fontsize=14)
+plt.ylabel("% of Fund Returns", color="white", fontproperties=ssp_font_semibold, fontsize=14)
+ax5.legend(loc='upper center', bbox_to_anchor=(0.5, -0.12), ncol=5,
+           frameon=False)
+plt.setp(ax5.get_legend().get_texts(), color='white', fontproperties=ssp_font_regular,
+         fontsize=12)
+plt.setp(ax5.get_xticklabels(), fontproperties=ssp_font_regular, fontsize=13)
+plt.setp(ax5.get_yticklabels(), fontproperties=ssp_font_regular, fontsize=13)
+st.pyplot(fig5)
+st.markdown("As you can see in the first chart, the composition of the \
+funds is relatively similar across each bucket. However, there is a dramatic \
+difference in terms of the breakdown of returns as seen in the second chart. The \
+vast majority of the returns for funds that at least breakeven comes from \
+'winning' companies that at least triple in value from initial investment to \
+exit. Nearly all the returns come from a small set of outlier investments.")
--- library.py
+++ library.py
@@ -2,12 +2,17 @@ import numpy as np
 import pandas as pd
 import powerlaw
 
+# FUNC: Given a target YoY growth rate and exit time, calculates the
+# corresponding alpha parameter for the power law distribution
 def calculate_alpha(target_yoy_growth, target_exit_time):
     # Convert annually compounded growth rate (YoY) to continuously compounded
     target_cont_comp_growth = np.log(1 + target_yoy_growth)
 
     return (1/(target_cont_comp_growth * target_exit_time) + 1)
 
+
+# FUNC: Simulates a single draw (i.e., one portfolio company) from the power law
+# distribution and predetermined probability distribution of return multiples
 def simulate_single_draw(alpha, liquidation_pct, prob_dist):
     # Ensure the probabilities sum to one by proportionally distributing
     # remainder across array
@@ -17,13 +22,16 @@ def simulate_single_draw(alpha, liquidation_pct, prob_dist):
     # Perform a weighted, random draw
     draw = np.random.choice(["ZERO", "LIQUIDATION", "MULTIPLE"], 1, p=prob_array)[0]
     if draw == "ZERO":
-        return 0
+        return 0.0
     elif draw == "LIQUIDATION":
         return liquidation_pct
     else:
         dist = powerlaw.Power_Law(xmin=1, parameters=[alpha])
         return dist.generate_random(1)[0]
 
+
+# FUNC: Simulates several venture funds with a set portfolio size each using the
+# simulate_single_draw function
 def simulate_multiple_funds(prob_dist, liquidation_pct, target_yoy_growth, target_exit_time, portfolio_size, simulation_runs=2500):
     # Calculate the appropriate alpha given the inputs
     alpha = calculate_alpha(target_yoy_growth, target_exit_time)
@@ -33,10 +41,63 @@ def simulate_multiple_funds(prob_dist, liquidation_pct, target_yoy_growth, targe
 
     return simulated_funds_list
 
-def calculate_overall_fund_returns(simulation_data):
-    overall_returns_list = []
+
+def calculate_raw_fund_returns(simulation_data):
+    raw_returns_list = []
     for fund in simulation_data:
         fund_array = np.array(fund)
-        fund_return = (fund_array.sum() - len(fund_array)) / len(fund_array)
-        overall_returns_list.append(fund_return)
-    return overall_returns_list
+        raw_fund_return = fund_array.sum() / len(fund_array)
+        raw_returns_list.append(raw_fund_return)
+    return raw_returns_list
+
+
+def calculate_actual_fund_returns(raw_returns_list, mgmt_pct_fee, fund_lifespan):
+    # Calculate what percentage of committed capital is leftover for actual investment
+    pct_capital_investment = 1.0 - (mgmt_pct_fee * fund_lifespan)
+
+    return(list(map(lambda x: x * pct_capital_investment, raw_returns_list)))
+
+
+def analyze_fund_returns(simulation_data, raw_returns_list, portfolio_size):
+   # Determine the bucket this fund gets sorted into
+   fund_analysis_list = []
+   for i in range(0, len(raw_returns_list)):
+       if (raw_returns_list[i] < 1):
+           fund_analysis_list.append({"bucket": "failure"})
+       elif (raw_returns_list[i] >=1) and (raw_returns_list[i] < 2):
+           fund_analysis_list.append({"bucket": "breakeven"})
+       elif (raw_returns_list[i] >=2) and (raw_returns_list[i] < 3):
+           fund_analysis_list.append({"bucket": "moderate_success"})
+       else:
+           fund_analysis_list.append({"bucket": "winner"})
+
+   # Calculate the composition of each fund's portfolio companies
+   for i in range(0, len(simulation_data)):
+       fund_analysis_list[i]["pct_comp_less_1x"] =  len([x for x in simulation_data[i] if x < 1]) / portfolio_size
+       fund_analysis_list[i]["pct_comp_1x_2x"] =  len([x for x in simulation_data[i] if (x >= 1) and (x < 2)]) / portfolio_size
+       fund_analysis_list[i]["pct_comp_2x_3x"] =  len([x for x in simulation_data[i] if (x >= 2) and (x < 3)]) / portfolio_size
+       fund_analysis_list[i]["pct_comp_greateq_3x"] =  len([x for x in simulation_data[i] if x >= 3]) / portfolio_size
+
+   # Calculate the composition of each fund's returns
+   for i in range(0, len(simulation_data)):
+       if (raw_returns_list[i] == 0):
+          fund_analysis_list[i]["pct_return_less_1x"] = 0.0
+          fund_analysis_list[i]["pct_return_1x_2x"] = 0.0
+          fund_analysis_list[i]["pct_return_2x_3x"] = 0.0
+          fund_analysis_list[i]["pct_return_greateq_3x"] = 0.0
+       else:
+          fund_analysis_list[i]["pct_return_less_1x"] = np.sum([x for x in simulation_data[i] if x < 1]) / np.sum(simulation_data[i])
+          fund_analysis_list[i]["pct_return_1x_2x"] = np.sum([x for x in simulation_data[i] if (x >= 1) and (x < 2)]) / np.sum(simulation_data[i])
+          fund_analysis_list[i]["pct_return_2x_3x"] = np.sum([x for x in simulation_data[i] if (x >= 2) and (x < 3)]) / np.sum(simulation_data[i])
+          fund_analysis_list[i]["pct_return_greateq_3x"] = np.sum([x for x in simulation_data[i] if x >= 3]) / np.sum(simulation_data[i])
+
+   return fund_analysis_list
+
+
+def get_averages_for_variable_across_buckets(fund_analysis_list, var):
+    bucket_list = ['failure', 'breakeven', 'moderate_success', 'winner']
+    results = []
+    for bucket in bucket_list:
+        result = np.average([x[var] for x in fund_analysis_list if x['bucket'] == bucket])
+        results.append(result)
+    return np.array(results)
